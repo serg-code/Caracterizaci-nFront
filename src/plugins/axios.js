512e570e5e2036f483b2409d457c791567bbc111
krlit <krlitozagusto@hotmail.es>
2023-01-19 16:25:22 -0500

    axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import router from "../router";
-import store from "../store";
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from '../router'
+import store from '../store'
 
-// axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}:8000/api`
-axios.defaults.baseURL = `http://sosaludaps.backend.test/api`;
-Vue.use(VueAxios, axios);
+axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}:8000/api`
+// axios.defaults.baseURL = `http://sosaludaps.backend.test/api`
+Vue.use(VueAxios, axios)
 
 axios.interceptors.response.use(undefined, async (error) => {
   if (error.response.status === 401) {
-    // store.commit('authModule/SET_LOGOUT')
-    store.commit("snackbar/set", {
-      color: "warning",
+    store.commit('authModule/SET_LOGOUT')
+    store.commit('snackbar/set', {
+      color: 'warning',
       message: `Los datos de la sesión han expirado, inicie sesión nuevamente.`,
-    });
-    await router.replace({ name: "Login" });
+    })
+    await router.replace({ name: 'Login' })
   }
-  return Promise.reject(error);
-});
+  return Promise.reject(error)
+})
